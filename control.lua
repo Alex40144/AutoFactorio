1f729c58eab869665c0f361e1e9311f081a38167
Alex40144 <giddyraccoon@gmail.com>
2020-07-27 12:24:26 +0100

    walking, mining and placing all working

--- control.lua
+++ control.lua
@@ -10,11 +10,20 @@ function debug(msg)
     if dbg then
         game.print(msg)
     end
+    return true
+end
+
+function debugtable(msg)
+    if dbg then
+        game.print(serpent.line(msg))
+    end
+    return true
 end
 
 function craft(p, item, count)
     p.begin_crafting{recipe = item, count = count}
-    debug(string.format("crafting %f %f", count, item))
+    debug(string.format("crafting " .. count .. " " .. item))
+    return true
 end
 
 function walk(p, deltax, deltay)
@@ -45,47 +54,68 @@ function walk(p, deltax, deltay)
 end
 
 function mine(p, location)
-    p.update_selected_entity(location)
-    p.mining_state = {mining = true, position = location}
+    if p.can_place_entity{name = "transport-belt", position = location, direction = defines.direction.north} then
+        return true
+    else
+        p.update_selected_entity(location)
+        p.mining_state = {mining = true, position = location}
+    end
 end
 
-function build(p, position, item, direction)
-    -- Build things
+function build(p, location, item, direction)
+    if p.can_place_entity{name = item, position = location, direction = direction} then
+        p.surface.create_entity{name = item, position = location, direction = direction, force="player"}
+        return true
+    end
 end
 
 function doTask(p, pos, tasks)
+    --debugtable(tasks)
     if tasks[1] == "build" then
+        --Build
         debug("build task started")
-        build(p, tasks[2], tasks[3], tasks[4])
+        return build(p, tasks[2], tasks[3], tasks[4])
+
     elseif tasks[1] == "craft" then
+        --craft
         debug("crafting task started")
-        craft(p,tasks[2], tasks[3])
+        return craft(p,tasks[2], tasks[3])
+
+    elseif tasks[1] == "mine" then
+        debug("mining task started")
+        return mine(p, tasks[2])
     elseif tasks[1] == "debug" then
-        debug(string.format("Current run time %f seconds", p.online_time / 60))
-        return false
+        --output current run time
+        return debug(string.format("Current run time %f seconds", p.online_time / 60))
+
     elseif tasks[1] == "walk" then
+        --walk
         debug("walking task started")
-        destination = {x=task[current_task].x, y=task[current_task].y}
+        destination = tasks[2]
         local destinationdelta = {x=destination.x - pos.x, y=destination.y - pos.y}
         return walk(p, destinationdelta.x, destinationdelta.y)
+
     elseif tasks[1] == "end" then
+        --end the run
+        debug("ending run")
         return "end"
+
     end
 end
 
 script.on_event(defines.events.on_tick, function(event)
+    --get player
     local p = game.players[1]
+    --get player's position
     local pos = p.position
 
-    --enable on first run
-
+    --only run if we are allowed to
     if enabled == true then
-        --if we are already walking
         if p.walking_state.walking == false then
             --if we are stopped
             result = doTask(p, pos, task[current_task])
             if result ~= nil then
-                if result.walking ~= nil then
+                if (type(result) == "table") then
                     if result.walking == true then
                         --do we want to continue walking
                         walking = result
@@ -93,7 +123,7 @@ script.on_event(defines.events.on_tick, function(event)
                         --stopping walking
                         current_task = current_task + 1
                     end
-                elseif result == false then
+                elseif result == true then
                     current_task = current_task + 1
                 elseif result == "end" then
                     enabled = false
@@ -102,7 +132,7 @@ script.on_event(defines.events.on_tick, function(event)
 
 
         else
-            --continue to walk
+            --we are still walking so let's continue to walk
             walking = walk(p, destination.x-pos.x, destination.y-pos.y)
             -- if the next task is building, then we can do that whilst moving.
         end
--- tasks.lua
+++ tasks.lua
@@ -1,8 +1,13 @@
 local task = {}
 
-task[1] = {"walk", x=20.0, y=20.0}
-task[2] = {"debug"}
-task[3] = {"end"}
+task[1] = {"walk", {x=-5,y=-21}}
+task[2] = {"mine", {x=-6,y=-24}}
+task[3] = {"craft", "stone-furnace", 8}
+task[4] = {"walk", {x=-20,y=27}}
+task[5] = {"build", {x=-22, y=29}, "burner-mining-drill", defines.direction.east}
+task[6] = {"build", {x=-20,y=29}, "stone-furnace", defines.direction.south}
+task[7] = {"debug"}
+task[8] = {"end"}
 
 
 return task
