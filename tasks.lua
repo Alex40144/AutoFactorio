f03c35f149c95d2413f35850df9a4aca934e846c
Alex40144 <giddyraccoon@gmail.com>
2020-07-28 15:16:47 +0100

    done some more scripting

--- tasks.lua
+++ tasks.lua
@@ -1,12 +1,223 @@
 local task = {}
 task[0] = {"walk", {x=-5,y=-21}}
 task[1] = {"mine", {x=-6,y=-24}}
-task[2] = {"craft", "stone-furnace", 8}
-task[3] = {"walk", {x=-20,y=27}}
+task[2] = {"craft", "stone-furnace", -1}
+task[3] = {"walk", {x=-21,y=28}}
 task[4] = {"build", {x=-22, y=29}, "burner-mining-drill", defines.direction.east}
 task[5] = {"build", {x=-20,y=29}, "stone-furnace", defines.direction.south}
 task[6] = {"put", "coal", 5, {x=-22, y=29}, defines.inventory.fuel}
-task[7] = {"put", "coal", 3, {x=-20, y=29}, defines.inventory.fuel}
-task[8] = {"debug"}
-task[9] = {"end"}
+task[7] = {"put", "coal", 5, {x=-20, y=29}, defines.inventory.fuel}
+task[8] = {"craft", "iron-gear-wheel", 3}
+task[9] = {"walk", {x=-9,y=22.5}}
+task[10] = {"mine", {x=-9,y=24}}
+task[11] = {"mine", {x=-8,y=20}}
+task[12] = {"walk", {x=-18,y=33}}
+task[13] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[14] = {"craft", "burner-mining-drill", 1}
+task[15] = {"build", {x=-20,y=31}, "stone-furnace", defines.direction.south}
+task[16] = {"build", {x=-20,y=33}, "stone-furnace", defines.direction.south}
+task[17] = {"build", {x=-20,y=35}, "stone-furnace", defines.direction.south}
+task[18] = {"build", {x=-20,y=37}, "stone-furnace", defines.direction.south}
+task[19] = {"build", {x=-22, y=31}, "burner-mining-drill", defines.direction.east}
+task[20] = {"put", "coal", 5, {x=-22, y=29}, defines.inventory.fuel}
+task[21] = {"put", "coal", 5, {x=-20, y=29}, defines.inventory.fuel}
+task[22] = {"put", "coal", 5, {x=-22, y=31}, defines.inventory.fuel}
+task[23] = {"put", "coal", 5, {x=-20, y=31}, defines.inventory.fuel}
+task[24] = {"walk", {x=-1,y=-30}}
+task[25] = {"mine", {x=0.5,y=-30}}
+task[26] = {"walk", {x=0,y=-30}}
+task[27] = {"mine", {x=3,y=-30}}
+task[28] = {"mine", {x=0,y=-32}}
+task[29] = {"craft", "stone-furnace", -1}
+task[30] = {"walk", {x=-18,y=33}}
+task[31] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[32] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[33] = {"craft", "burner-mining-drill", 1}
+task[34] = {"build", {x=-22, y=33}, "burner-mining-drill", defines.direction.east}
+task[35] = {"put", "coal", 5, {x=-20, y=33}, defines.inventory.fuel}
+task[36] = {"put", "coal", 5, {x=-22, y=33}, defines.inventory.fuel}
+task[37] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[38] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[39] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[40] = {"craft", "burner-mining-drill", 1}
+task[41] = {"build", {x=-22, y=35}, "burner-mining-drill", defines.direction.east}
+task[42] = {"put", "coal", 5, {x=-20, y=35}, defines.inventory.fuel}
+task[43] = {"put", "coal", 5, {x=-22, y=35}, defines.inventory.fuel}
+task[44] = {"walk", {x=-21,y=42}}
+task[45] = {"mine", {x=-23.5,y=43}}
+task[46] = {"walk", {x=-18,y=33}}
+task[47] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[48] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[49] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[50] = {"take", {x=-20, y=35}, "iron-plate", -1, defines.inventory.furnace_result}
+task[51] = {"craft", "burner-mining-drill", 1}
+task[52] = {"build", {x=-22, y=37}, "burner-mining-drill", defines.direction.east}
+task[53] = {"put", "coal", 10, {x=-20, y=37}, defines.inventory.fuel}
+task[54] = {"put", "coal", 10, {x=-22, y=37}, defines.inventory.fuel}
+task[55] = {"put", "coal", 5, {x=-20, y=35}, defines.inventory.fuel}
+task[56] = {"put", "coal", 5, {x=-22, y=35}, defines.inventory.fuel}
+task[57] = {"put", "coal", 5, {x=-20, y=33}, defines.inventory.fuel}
+task[58] = {"put", "coal", 5, {x=-22, y=33}, defines.inventory.fuel}
+task[59] = {"put", "coal", 5, {x=-22, y=31}, defines.inventory.fuel}
+task[60] = {"put", "coal", 5, {x=-20, y=31}, defines.inventory.fuel}
+task[61] = {"put", "coal", 5, {x=-22, y=29}, defines.inventory.fuel}
+task[62] = {"put", "coal", 5, {x=-20, y=29}, defines.inventory.fuel}
+task[63] = {"walk", {x=9,y=76}}
+task[64] = {"mine", {x=8,y=78}}
+task[65] = {"walk", {x=15,y=75}}
+task[66] = {"mine", {x=17,y=76}}
+task[67] = {"craft", "stone-furnace", -1}
+task[68] = {"walk", {x=-18,y=33}}
+task[69] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[70] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[71] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[72] = {"take", {x=-20, y=35}, "iron-plate", -1, defines.inventory.furnace_result}
+task[73] = {"take", {x=-20, y=37}, "iron-plate", -1, defines.inventory.furnace_result}
+task[74] = {"craft", "burner-mining-drill", -1}
+task[75] = {"walk", {x=-12.5,y=73.5}}
+task[76] = {"mine", {x=-13.5,y=75.5}}
+task[77] = {"mine", {x=-11,y=74.5}}
+task[78] = {"walk", {x=-18,y=37}}
+task[79] = {"build", {x=-20,y=47}, "stone-furnace", defines.direction.south}
+task[80] = {"build", {x=-22, y=47}, "burner-mining-drill", defines.direction.east}
+task[81] = {"put", "coal", 5, {x=-20, y=47}, defines.inventory.fuel}
+task[82] = {"put", "coal", 5, {x=-22, y=47}, defines.inventory.fuel}
+task[83] = {"build", {x=-20,y=45}, "stone-furnace", defines.direction.south}
+task[84] = {"build", {x=-22, y=45}, "burner-mining-drill", defines.direction.east}
+task[85] = {"put", "coal", 5, {x=-20, y=45}, defines.inventory.fuel}
+task[86] = {"put", "coal", 5, {x=-22, y=45}, defines.inventory.fuel}
+task[87] = {"build", {x=-20,y=43}, "stone-furnace", defines.direction.south}
+task[88] = {"build", {x=-22, y=43}, "burner-mining-drill", defines.direction.east}
+task[89] = {"put", "coal", 5, {x=-20, y=43}, defines.inventory.fuel}
+task[90] = {"put", "coal", 5, {x=-22, y=43}, defines.inventory.fuel}
+task[91] = {"build", {x=-20,y=41}, "stone-furnace", defines.direction.south}
+task[92] = {"build", {x=-22, y=41}, "burner-mining-drill", defines.direction.east}
+task[93] = {"put", "coal", 5, {x=-20, y=41}, defines.inventory.fuel}
+task[94] = {"put", "coal", 5, {x=-22, y=41}, defines.inventory.fuel}
+task[95] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[96] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[97] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[98] = {"take", {x=-20, y=35}, "iron-plate", -1, defines.inventory.furnace_result}
+task[99] = {"take", {x=-20, y=37}, "iron-plate", -1, defines.inventory.furnace_result}
+task[100] = {"craft", "burner-mining-drill", -1}
+task[101] = {"build", {x=-20,y=39}, "stone-furnace", defines.direction.south}
+task[102] = {"build", {x=-22, y=39}, "burner-mining-drill", defines.direction.east}
+task[103] = {"put", "coal", 5, {x=-20, y=39}, defines.inventory.fuel}
+task[104] = {"put", "coal", 5, {x=-22, y=39}, defines.inventory.fuel}
+task[105] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[106] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[107] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[108] = {"take", {x=-20, y=35}, "iron-plate", -1, defines.inventory.furnace_result}
+task[109] = {"take", {x=-20, y=37}, "iron-plate", -1, defines.inventory.furnace_result}
+task[110] = {"craft", "burner-mining-drill", -1}
+task[111] = {"walk", {x=-4,y=30}}
+task[112] = {"mine", {x=-2.5,y=31.5}}
+task[113] = {"mine", {x=-2.5,y=29}}
+task[114] = {"walk", {x=1,y=36}}
+task[115] = {"mine", {x=0,y=34}}
+task[116] = {"mine", {x=2,y=37}}
+task[117] = {"walk", {x=-18,y=45}}
+task[118] = {"take", {x=-20, y=47}, "iron-plate", -1, defines.inventory.furnace_result}
+task[119] = {"take", {x=-20, y=45}, "iron-plate", -1, defines.inventory.furnace_result}
+task[120] = {"take", {x=-20, y=43}, "iron-plate", -1, defines.inventory.furnace_result}
+task[121] = {"take", {x=-20, y=41}, "iron-plate", -1, defines.inventory.furnace_result}
+task[122] = {"take", {x=-20, y=39}, "iron-plate", -1, defines.inventory.furnace_result}
+task[123] = {"walk", {x=-18,y=26}}
+task[124] = {"take", {x=-20, y=37}, "iron-plate", -1, defines.inventory.furnace_result}
+task[125] = {"take", {x=-20, y=35}, "iron-plate", -1, defines.inventory.furnace_result}
+task[126] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[127] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[128] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[129] = {"craft", "burner-mining-drill", -1}
+task[130] = {"walk", {x=-16.5,y=16.5}}
+task[131] = {"build", {x=-17.5, y=13.5}, "burner-mining-drill", defines.direction.west}
+task[132] = {"build", {x=-19.5, y=13.5}, "burner-mining-drill", defines.direction.north}
+task[133] = {"build", {x=-19.5, y=11.5}, "burner-mining-drill", defines.direction.east}
+task[134] = {"build", {x=-17.5, y=11.5}, "burner-mining-drill", defines.direction.south}
+task[135] = {"put", "coal", 1, {x=-17.5, y=13.5}, defines.inventory.fuel}
+task[136] = {"put", "coal", 1, {x=-19.5, y=13.5}, defines.inventory.fuel}
+task[137] = {"put", "coal", 1, {x=-19.5, y=11.5}, defines.inventory.fuel}
+task[138] = {"put", "coal", 1, {x=-17.5, y=11.5}, defines.inventory.fuel}
+task[139] = {"walk", {x=-18,y=26}}
+task[140] = {"walk", {x=-18,y=49}}
+task[141] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[142] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[143] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[144] = {"take", {x=-20, y=35}, "iron-plate", -1, defines.inventory.furnace_result}
+task[145] = {"take", {x=-20, y=37}, "iron-plate", -1, defines.inventory.furnace_result}
+task[146] = {"take", {x=-20, y=39}, "iron-plate", -1, defines.inventory.furnace_result}
+task[147] = {"take", {x=-20, y=41}, "iron-plate", -1, defines.inventory.furnace_result}
+task[148] = {"take", {x=-20, y=43}, "iron-plate", -1, defines.inventory.furnace_result}
+task[149] = {"take", {x=-20, y=45}, "iron-plate", -1, defines.inventory.furnace_result}
+task[150] = {"take", {x=-20, y=47}, "iron-plate", -1, defines.inventory.furnace_result}
+task[151] = {"walk", {x=-18,y=49}}
+task[152] = {"craft", "burner-mining-drill", -1}
+task[153] = {"walk", {x=-24,y=49}}
+task[154] = {"walk", {x=-24,y=30}}
+task[155] = {"build", {x=-25,y=29}, "stone-furnace", defines.direction.south}
+task[156] = {"build", {x=-27, y=29}, "burner-mining-drill", defines.direction.east}
+task[157] = {"build", {x=-25,y=27}, "stone-furnace", defines.direction.south}
+task[158] = {"build", {x=-27, y=27}, "burner-mining-drill", defines.direction.east}
+task[159] = {"put", "coal", 5, {x=-25, y=29}, defines.inventory.fuel}
+task[160] = {"put", "coal", 5, {x=-27, y=29}, defines.inventory.fuel}
+task[161] = {"put", "coal", 5, {x=-25, y=27}, defines.inventory.fuel}
+task[162] = {"put", "coal", 5, {x=-27, y=27}, defines.inventory.fuel}
+task[163] = {"walk", {x=-5,y=20}}
+task[164] = {"mine", {x=-7,y=19}}
+task[165] = {"mine", {x=-4,y=19}}
+task[166] = {"walk", {x=3,y=26}}
+task[167] = {"mine", {x=4,y=26}}
+task[168] = {"mine", {x=3,y=25}}
+task[169] = {"walk", {x=2,y=36}}
+task[170] = {"mine", {x=2,y=38}}
+task[171] = {"walk", {x=-18,y=45}}
+task[172] = {"walk", {x=-18,y=26}}
+task[173] = {"take", {x=-20, y=47}, "iron-plate", -1, defines.inventory.furnace_result}
+task[174] = {"take", {x=-20, y=45}, "iron-plate", -1, defines.inventory.furnace_result}
+task[175] = {"take", {x=-20, y=43}, "iron-plate", -1, defines.inventory.furnace_result}
+task[176] = {"take", {x=-20, y=41}, "iron-plate", -1, defines.inventory.furnace_result}
+task[177] = {"take", {x=-20, y=39}, "iron-plate", -1, defines.inventory.furnace_result}
+task[178] = {"take", {x=-20, y=37}, "iron-plate", -1, defines.inventory.furnace_result}
+task[179] = {"take", {x=-20, y=35}, "iron-plate", -1, defines.inventory.furnace_result}
+task[180] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[181] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[182] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[183] = {"craft", "burner-mining-drill", 1}
+task[184] = {"craft", "burner-mining-drill", 1}
+task[185] = {"craft", "burner-mining-drill", 1}
+task[186] = {"craft", "burner-mining-drill", -1}
+task[187] = {"walk", {x=-24,y=26}}
+task[188] = {"walk", {x=-24,y=36}}
+task[189] = {"build", {x=-25, y=31}, "stone-furnace", defines.direction.south}
+task[190] = {"build", {x=-27, y=31}, "burner-mining-drill", defines.direction.east}
+task[191] = {"put", "coal", 5, {x=-25, y=31}, defines.inventory.fuel}
+task[192] = {"put", "coal", 5, {x=-27, y=31}, defines.inventory.fuel}
+task[193] = {"build", {x=-25, y=33}, "stone-furnace", defines.direction.south}
+task[194] = {"build", {x=-27, y=33}, "burner-mining-drill", defines.direction.east}
+task[195] = {"put", "coal", 5, {x=-25, y=33}, defines.inventory.fuel}
+task[196] = {"put", "coal", 5, {x=-27, y=33}, defines.inventory.fuel}
+task[197] = {"build", {x=-25, y=35}, "stone-furnace", defines.direction.south}
+task[198] = {"build", {x=-27, y=35}, "burner-mining-drill", defines.direction.east}
+task[199] = {"put", "coal", 5, {x=-25, y=35}, defines.inventory.fuel}
+task[200] = {"put", "coal", 5, {x=-27, y=35}, defines.inventory.fuel}
+task[201] = {"build", {x=-25, y=37}, "stone-furnace", defines.direction.south}
+task[202] = {"build", {x=-27, y=37}, "burner-mining-drill", defines.direction.east}
+task[203] = {"put", "coal", 5, {x=-25, y=37}, defines.inventory.fuel}
+task[204] = {"put", "coal", 5, {x=-27, y=37}, defines.inventory.fuel}
+task[205] = {"walk", {x=-24,y=49}}
+task[206] = {"walk", {x=-18,y=49}}
+task[207] = {"walk", {x=-18,y=26}}
+task[208] = {"take", {x=-20, y=47}, "iron-plate", -1, defines.inventory.furnace_result}
+task[209] = {"take", {x=-20, y=45}, "iron-plate", -1, defines.inventory.furnace_result}
+task[210] = {"take", {x=-20, y=43}, "iron-plate", -1, defines.inventory.furnace_result}
+task[211] = {"take", {x=-20, y=41}, "iron-plate", -1, defines.inventory.furnace_result}
+task[212] = {"take", {x=-20, y=39}, "iron-plate", -1, defines.inventory.furnace_result}
+task[213] = {"take", {x=-20, y=37}, "iron-plate", -1, defines.inventory.furnace_result}
+task[214] = {"take", {x=-20, y=35}, "iron-plate", -1, defines.inventory.furnace_result}
+task[215] = {"take", {x=-20, y=33}, "iron-plate", -1, defines.inventory.furnace_result}
+task[216] = {"take", {x=-20, y=31}, "iron-plate", -1, defines.inventory.furnace_result}
+task[217] = {"take", {x=-20, y=29}, "iron-plate", -1, defines.inventory.furnace_result}
+task[218] = {"craft", "burner-mining-drill", -1}
+task[219] = {"time"}
+task[220] = {"end"}
 return task
